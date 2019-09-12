2e74370bc14669759b037c93779ef260a333f920
Yueeeeeeee <yuezrhb@gmail.com>
2019-09-12 18:30:40 +0200

    update Doosan

--- doosanAllMachineSpecs.py
+++ doosanAllMachineSpecs.py
@@ -1,94 +1,77 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from xlwt import *
-import xlrd
 import re
+import time
+from xlwt import *
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-def outputExcel(url):
-    urlIterator = iter(url)
-    excelFile = Workbook(encoding='utf-8')
-    excelTable = excelFile.add_sheet('Doosan')
+def writeData(labelList, curlData, rowNum, table):
+    dataList = []
+    excelTable.write(rowNum, 0, curlData.find('p', attrs={"class": "name"}).string)
+    temp = curlData.find('p', attrs={"class": re.compile('specValOrd\d{1,2}')})
+    colNum = 1
 
-    row = 0
-    for i in urlIterator:
-        if(i == ''): break
+    while temp is not None:
+        if 'display: none' in str(temp):
+            temp = temp.find_next('p', attrs={"class": re.compile('specValOrd\d{1,2}')}) # avoid looping when entering a none-display element
+            continue
 
-        htmlMachine = urlopen(i).read().decode('utf-8')
-        soupMachine = BeautifulSoup(htmlMachine, 'lxml')
+        if temp.string is not None:
+            dataList.append(temp.string.replace(' ', '').replace('\n', '').replace('\r', '').replace('\t', ''))
+        else:
+            dataList.append('')
+
+        temp = temp.find_next('p', attrs={"class": re.compile('specValOrd\d{1,2}')})
+
+    for j in range(len(labelList)):
+        table.write(rowNum, 2 * colNum - 1, labelList[j])
+        table.write(rowNum, 2 * colNum, dataList[j])
+        colNum += 1
 
 
-        div = soupMachine.find_all('table', {'class': 'dataW_table'}) # spec table
 
-        # head name(s)
-        head1 = re.findall(r'<th scope="col" class="right">.*</th>', str(div)) # in case the scope and class are inverted
-        head2 = re.findall(r'<th class="right" scope="col">.*</th>', str(div)) # in case the scope and class are inverted
-        head3 = head1 + head2
+urlList = ['https://www.doosanmachinetools.com/en/product/series/D221_69/view.do', 'https://www.doosanmachinetools.com/en/product/series/D203_53/view.do']
 
-        headNum = len(head3)
-        heads = []
-        headIterator = iter(head3)
-        for ihead in headIterator:
-            heads.append(str(ihead)[30:-5]) # final machine names
+excelFile = Workbook(encoding='utf-8')
+excelTable = excelFile.add_sheet('Doosan')
 
-        # extract spec table information
-        spec1 = re.findall(r'<td class="tit">.*</td>', str(div), re.DOTALL)
-        spec2 = str(spec1).replace('\\t', "").replace('\\r', "").replace('\\n', "").replace('  ', "") # replace all new lines, tabs etc
-        specs = re.sub(r'\<(.*?[^\<][^\>])\>', ' ', spec2).split('  ') # delete all brackets and content within, then split contents into a list
+optionChrome = Options()
+optionChrome.add_argument('--headless')
+optionChrome.add_argument('--disable-gpu')
+optionChrome.add_argument('disable-plugins')
+optionChrome.add_argument('disable-extensions')
 
-        # remove starting blanks, "[' "s and empty elements
-        num = 0
-        while num < len(specs):
-            if (len(specs[num]) == 0):
-                del specs[num]
-                num = num - 1
-            else:
-                if(specs[num][0] == ' '):
-                    specs[num] = specs[num][1:]
-                if(specs[num][0] == '['):
-                    specs[num] = specs[num][3:]
-            num = num + 1
+urlIterator = iter(urlList)
+row = 0
 
-        # start writing excel file
-        rowElementNumber = headNum + 2
-        num = 0 # machine number on the same page
+for u in urlIterator:
+    driverChrome = webdriver.Chrome(options=optionChrome)
+    driverChrome.get(u)
+    time.sleep(2)
+    htmlResult = driverChrome.page_source
+    driverChrome.quit()
 
-        while num < headNum:
-            column = 0  # column number
-            excelTable.write(row, column, heads[num])  # write machine name in 1. column
-            print("writing column: " + str(column) + ", row: " + str(row) + ", content: name")  # console output
-            column = column + 1
+    soup = BeautifulSoup(htmlResult, 'lxml')
 
-            specPose = 0 # starting writing specs
-            while specPose < len(specs):
-                if(specPose % rowElementNumber == 0): # write classification
-                    excelTable.write(row, column, specs[specPose])
-                    print("writing column: " + str(column) + ", row: " + str(row) + ", content: classification")  # console output
-                    column = column + 1
-                elif(specPose % rowElementNumber == 1): # skip unit
-                    print("skip unit")
-                else: # write specs with unit
-                    if(specPose % rowElementNumber == num + 2):
-                        excelTable.write(row, column, specs[specPose] + " " + specs[specPose - num - 1])  # write spec and unit
-                        print("writing column: " + str(column) + ", row: " + str(row) + ", content: specs")  # console output
-                        column = column + 1
+    labelList = []
+    curlLabel = soup.find('div', attrs={"class": "fixedArea"})
+    pLabel = curlLabel.find('p', attrs={"class": re.compile('specOrd\d{1,2}')})
 
-                specPose = specPose + 1 # next position
+    while pLabel is not None:
+        if 'display: none' in str(pLabel):
+            pLabel = pLabel.find_next('p', attrs={
+                "class": re.compile('specOrd\d{1,2}')})  # avoid looping when entering a none-display element
+            continue
 
-            num = num + 1 # next machine (if any)
-            row = row + 1 # change to next row
+        labelList.append(pLabel.string)
+        pLabel = pLabel.find_next('p', attrs={"class": re.compile('specOrd\d{1,2}')})
 
-    excelFile.save('Doosan_MachineData.xls')
 
-# start of code
-# open excel file
-excelURLFile = xlrd.open_workbook('Doosan_URLList.xls')
-excelTable = excelURLFile.sheet_by_name('Doosan_URLList')
+    curlData = soup.find('div', attrs={"class": "scrollArea"})
+    divData = curlData.find('div', attrs={"class": "productList"})
+    while divData is not None:
+        writeData(labelList, divData, row, excelTable)
+        row += 1
+        divData = divData.find_next('div', attrs={"class": "productList"})
 
-# add all entries in a urlList
-urlList = []
-i = 0
-while i < excelTable.nrows:
-    urlList.append(excelTable.cell(i, 0).value)
-    i = i + 1
-
-outputExcel(urlList)
+excelFile.save('Doosan_MachineData.xls')
